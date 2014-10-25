3acf4691d7026ff245dcf1e148f8c433c889b51a
Theodore X. Pak <mail@theopak.com>
2014-10-25 17:39:59 -0400

    More standardization around Weka API.

--- src/HwMain.java
+++ src/HwMain.java
@@ -7,129 +7,41 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.FastVector;
 import weka.core.Attribute;
-
+import weka.core.Utils;
+import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.trees.*;
 import weka.classifiers.rules.*;
 import weka.classifiers.bayes.*;
 import weka.classifiers.functions.*;
 import weka.classifiers.lazy.IBk;
+import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.filters.Filter;
 
 public class HwMain {
 	public static void main(String[] args) throws Exception{
-		
-		// Options and java sttributes
+		// Options and java attributes
 		String pathFile = "/home/micah/courses/affective_computing/hw3-2/postureData.txt";
-		pathFile = "/Users/theopak/Dropbox/classes/csci-4974_affective-computing/postureData.csv";
-		int nInstance = 0;	// number of instances in the data set
-		int nAttribute = 0;	// number of attributes in the data set
+		pathFile = "/Users/theopak/Dropbox/classes/csci-4974_affective-computing/postureData.arff";
 		int seed  = 4;		// chosen by fair dice roll, guaranteed to be random.
 		int folds = 10;		// number of folds in cross-validation
 		
-		//loading the file
-		BufferedReader br = new BufferedReader(new FileReader(pathFile));
-		String line;
-		//int nInstance = 0;
-		ArrayList<ArrayList<Double>> data = new ArrayList<ArrayList<Double> >();
-		String[] attributes = null;
-		while ((line = br.readLine()) != null) {
-			if (nInstance == 0) { 
-				//The first line is the name of attributes
-				attributes = line.split(",");
-			} else {
-				String[] value = line.split(",");
-				ArrayList<Double> temp = new ArrayList<Double>();
-				for (int i = 0; i < value.length; i++) {
-					temp.add(Double.parseDouble(value[i]));
-				}
-				data.add(temp);
-			}
-			nInstance++;
-		}
-		nInstance--; //This is the number of instance
-		nAttribute = attributes.length;
-		br.close();
-
-		//Print out the whole data file.
-		System.out.println("Number of attributes: " + nAttribute);
-		System.out.println("Number of Instance: " + nInstance);
-		for (int i = 0; i < nAttribute; i++) {
-			System.out.print(attributes[i] + " ");
-		}
+		// Load data set from file using the recommended method for 
+		//   ARFF files in Weka > 3.5.5. For details refer to:
+		//   http://weka.wikispaces.com/Use+WEKA+in+your+Java+code
+		DataSource source = new DataSource(pathFile);
+		Instances data = source.getDataSet();
+		if (data.classIndex() == -1)
+		   data.setClassIndex(data.numAttributes() - 1);
+		
+		// Print the data set USING THE WEKA API THAT ALREADY DOES THE WORK
+		//  FOR YOU WHY WOULD YOU DO IT ANY OTHER WAY !?
+		System.out.println(data.toSummaryString());
 		System.out.print("\n");
-		for (int i = 0; i < nInstance; i++) {
-			for (int j = 0; j < nAttribute; j++) {
-				System.out.print((data.get(i)).get(j) + " ");
-			}
-			System.out.print("\n");
-		}
-		System.out.print("\n");
-		
-		/////////////////////////////////////////////////////////////////////////
-		/*
-		* The last attribute is label which is what we want to predict
-		* There are 4 labels - 1 2 3 4
-		* 1 = Triumphant
-		* 2 = Concentrated
-		* 3 = Defeated
-		* 4 = Frustrated
-		*/
-		
-		//---Building features in weka format---//
-		
-		//Declare numeric attributes (weka.core.Attribute)
-		Attribute Attribute1 = new Attribute("XpositionHip");
-		Attribute Attribute2 = new Attribute("YpositionHip");
-		Attribute Attribute3 = new Attribute("ZpositionHip");
-		//You should come up with any new feature
-		//For example, the size of this vector, sqrt(x^2 + y^2 + z^2), name it sumHip.
-		
-		//Declare the class attribute
-		//Declare a nominal attribute along with its value 
-		//(weka.core.FastVector)
-		FastVector fvClassVal = new FastVector(4); //4 different class
-		//you can also use the name of emotion
-		fvClassVal.addElement("1");
-		fvClassVal.addElement("2");
-		fvClassVal.addElement("3");
-		fvClassVal.addElement("4");
-		Attribute ClassAttribute = new Attribute("Emotion", fvClassVal);
-		
-		//Declare the feature vector
-		FastVector fvWekaAttributes = new FastVector(4);
-		fvWekaAttributes.addElement(Attribute1);
-		fvWekaAttributes.addElement(Attribute2);
-		fvWekaAttributes.addElement(Attribute3);
-		fvWekaAttributes.addElement(ClassAttribute);
-		
-		//Create an empty training set (weka.core.Instances)
-		//						"name the relation",feature vector,initial set capacity
-		Instances train = new Instances("Posture",fvWekaAttributes,nInstance);
-		//set class index (the thing that we want to predict)
-		//index starts at 0.
-		train.setClassIndex(3);
-		
-		//adding instances to training set
-		for(int i = 0; i < nInstance; i++){
-			//Create the instance (weka.core.Instance)
-			Instance temp = new Instance(4);
-			//setValue( Attribute, value)
-			temp.setValue( (Attribute)fvWekaAttributes.elementAt(0), data.get(i).get(0) );
-			temp.setValue( (Attribute)fvWekaAttributes.elementAt(1), data.get(i).get(1) );
-			temp.setValue( (Attribute)fvWekaAttributes.elementAt(2), data.get(i).get(2) );
-			 
-			int classValue = (data.get(i).get(nAttribute-1)).intValue();
-			temp.setValue( (Attribute)fvWekaAttributes.elementAt(3), 
-					Integer.toString( classValue ) );
-			//add the instance
-			//classValue when print out is shown as index,not the value
-			//System.out.println(temp); 
-			train.add(temp);
-		}
 		
 		
 		//------Creating a classifier------//
-		System.out.println("The result of the classifier\n");
+		System.out.println("Classifying and validating using 10-fold CV\n");
 
 		// Classifier
 		String[] tmpOptions;
