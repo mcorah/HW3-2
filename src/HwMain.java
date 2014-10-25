91e65d85fbbfc4334ec84a2ebe8a67e4343abfee
Theodore X. Pak <mail@theopak.com>
2014-10-25 16:16:24 -0400

    Adds 10-folds Weka Cross-Validation classifier.
    Code from the `CrossValidationSingleRun.java` file via
    http://weka.wikispaces.com/Generating+cross-validation+folds+(Java+appro
    ach)

--- src/HwMain.java
+++ src/HwMain.java
@@ -124,5 +124,61 @@ public class HwMain {
 		
 		//------Creating a classifier------//
 		System.out.println("The result of the classifier\n");
+
+		// loads data and set class index
+		//Instances data(train);
+		String clsIndex = Utils.getOption("c", args);
+		if (clsIndex.length() == 0)
+			clsIndex = "last";
+		if (clsIndex.equals("first"))
+			data.setClassIndex(0);
+		else if (clsIndex.equals("last"))
+			data.setClassIndex(data.numAttributes() - 1);
+		else
+			data.setClassIndex(Integer.parseInt(clsIndex) - 1);
+
+		// classifier
+		String[] tmpOptions;
+		String classname;
+		tmpOptions     = Utils.splitOptions(Utils.getOption("W", args));
+		classname      = tmpOptions[0];
+		tmpOptions[0]  = "";
+		Classifier cls = (Classifier) Utils.forName(Classifier.class, classname, tmpOptions);
+
+		// other options
+		int seed  = Integer.parseInt(Utils.getOption("s", args));
+		int folds = Integer.parseInt(Utils.getOption("x", args));
+
+		// randomize data
+		Random rand = new Random(seed);
+		Instances randData = new Instances(data);
+		randData.randomize(rand);
+		if (randData.classAttribute().isNominal())
+			randData.stratify(folds);
+
+		// perform cross-validation
+		Evaluation eval = new Evaluation(randData);
+		for (int n = 0; n < folds; n++) {
+			Instances train = randData.trainCV(folds, n);
+			Instances test = randData.testCV(folds, n);
+			// the above code is used by the StratifiedRemoveFolds filter, the
+			// code below by the Explorer/Experimenter:
+			// Instances train = randData.trainCV(folds, n, rand);
+
+			// build and evaluate classifier
+			Classifier clsCopy = Classifier.makeCopy(cls);
+			clsCopy.buildClassifier(train);
+			eval.evaluateModel(clsCopy, test);
+		}
+
+		// output evaluation
+		System.out.println();
+		System.out.println("=== Setup ===");
+		System.out.println("Classifier: " + cls.getClass().getName() + " " + Utils.joinOptions(cls.getOptions()));
+		System.out.println("Dataset: " + data.relationName());
+		System.out.println("Folds: " + folds);
+		System.out.println("Seed: " + seed);
+		System.out.println();
+		System.out.println(eval.toSummaryString("=== " + folds + "-fold Cross-validation ===", false));
 	}
 }
