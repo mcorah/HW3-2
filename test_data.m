db044028792ac23583d500185b152145c19548e3
Andrew Cunningham <cunnia3@rpi.edu>
2014-10-25 17:27:50 -0400

    Changed test_data for matlab 2013a

--- test_data.m
+++ test_data.m
@@ -1,4 +1,4 @@
-data = readtable('postureData.txt');
+data = csvread('postureData.csv',1,0);
 
 labels = {'triumphant' 'concentrated' 'defeated' 'frustrated'};
 plotFeature(data.XpositionHip,data.Label);
