333f081530a9a56927f47ed8590d2ce6a9de68ca
Micah Corah <micahcorah@gmail.com>
2014-10-26 18:02:01 -0400

    more plotting commands

---
+++ plotLR.m
@@ -0,0 +1,4 @@
+function plotLR(feature, data)
+  plotXYZ(['rotationLeft' feature], data)
+  plotXYZ(['rotationRight' feature], data)
+end
---
+++ plotXYZ.m
@@ -0,0 +1,5 @@
+function plotXYZ(feature, data)
+  plotFeature(['Z' feature], data)
+  plotFeature(['X' feature], data)
+  plotFeature(['Y' feature], data)
+end
