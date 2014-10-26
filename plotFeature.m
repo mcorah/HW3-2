b825b5912f2c5a4cbe03dcb75b9efde6efb9005f
Micah Corah <micahcorah@gmail.com>
2014-10-26 16:10:32 -0400

    passing string to plotting function so title can be created

--- plotFeature.m
+++ plotFeature.m
@@ -1,13 +1,15 @@
-function plotFeature(feature, mask)
-  labels = unique(mask)
+function plotFeature(feature, data)
+  labels = unique(data.Label)
   colors = 'rbgy';
+  column = data{:,feature}
   figure();
   hold on
   for i=labels'
-    data = feature(find(mask==i))
-    plot(data,['*' colors(i)]);
+    plot_data = column(find(data.Label==i))
+    plot(plot_data,['*' colors(i)]);
   end
   xlabel('index');
   ylabel('value');
+  title(feature)
   hold off
 end
