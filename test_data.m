dc03ab8fefc787f42669b9edd2f7abc686e86403
Andrew Cunningham <cunnia3@rpi.edu>
2014-10-27 18:34:48 -0400

    Commented out lines that wont work with matlab 2013a

--- test_data.m
+++ test_data.m
@@ -1,4 +1,6 @@
 data = csvread('postureData.csv',1,0);
 
-labels = {'triumphant' 'concentrated' 'defeated' 'frustrated'};
-plotFeature(data.XpositionHip,data.Label);
+
+
+%labels = {'triumphant' 'concentrated' 'defeated' 'frustrated'};
+%plotFeature(data.XpositionHip,data.Label);
